381834241dd0964c65e9eb40a639de5dc2f0d890
BalamuruganSivanthi <balawpt@gmail.com>
2026-01-03 17:11:28 -0500

    added notebook screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
+import { NotebooksComponent } from './pages/notebooks/notebooks.component';
+
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'notebooks', component: NotebooksComponent }
 ];
