b556b1c6a209e9317743d52dec7e93c4c06b6c2d
BalamuruganSivanthi <balawpt@gmail.com>
2026-01-03 23:02:58 -0500

    addeed order page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 import { NotebooksComponent } from './pages/notebooks/notebooks.component';
+import { OrderComponent } from './pages/order/order.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'notebooks', component: NotebooksComponent }
+    { path: 'notebooks', component: NotebooksComponent },
+    { path: 'order/:id', component: OrderComponent }
 ];
--- src/app/pages/notebooks/notebooks.component.ts
+++ src/app/pages/notebooks/notebooks.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-notebooks',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './notebooks.component.html',
   styleUrl: './notebooks.component.css'
 })
